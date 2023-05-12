0cf7b916ee001a31db7a76ba369b0ad1d9e24c73
Soyeon <amy1038@naver.com>
2023-05-12 10:50:36 +0900

    setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} /> {/* 이 부분이 Layout의 children prop */}
+      </Layout>
+    </>
+  );
 }
